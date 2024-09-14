7e8bccd7b6905266111c4ad79a87216a83875a39
Mikhail Sirotyuk <donald.spongeputin@gmail.com>
2024-09-14 13:41:23 +0300

    close

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3641879353"
+    serviceWorkerVersion: "2358734113"
   }
 });
