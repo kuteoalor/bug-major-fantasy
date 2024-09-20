9e41ea39ddfd9d2ae1551a3dfa067af2cf4366f1
Mikhail Sirotyuk <donald.spongeputin@gmail.com>
2024-09-20 17:40:18 +0300

    size fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "358499241"
+    serviceWorkerVersion: "2064516499"
   }
 });
